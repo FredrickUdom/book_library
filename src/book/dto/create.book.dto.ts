c99c8002c0987b7e98191b3d60d7da07361138d5
“fredrickUdom” <fredrickudom@gmail.com>
2023-05-21 19:02:32 +0100

    adding validation pipes and mongooseDB ID validation

--- src/book/dto/create.book.dto.ts
+++ src/book/dto/create.book.dto.ts
@@ -1,10 +1,24 @@
+import { IsCreditCard, IsEnum, IsNotEmpty, IsNumber, IsString } from "class-validator";
 import { Category } from "../enum/category.enum";
 
 export class createBookDTO{
+    @IsNotEmpty()
+    @IsString()
     readonly title: string;
-    readonly description: string;
-    readonly author: string;
-    readonly price: number;
-    readonly category: Category
 
+    @IsNotEmpty()
+    @IsString()
+    readonly description: string;
+
+    @IsNotEmpty()
+    @IsString()
+    readonly author: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    readonly price: number;
+
+    @IsNotEmpty()
+    @IsEnum(Category,{message:'please enter the correct enum'})
+    readonly category: Category;
 }
--- src/book/dto/update.book.dto.ts
+++ src/book/dto/update.book.dto.ts
@@ -1,10 +1,25 @@
+import { IsEnum, IsNumber, IsOptional, IsString } from "class-validator";
 import { Category } from "../enum/category.enum";
 
 export class updateBookDTO{
+    @IsOptional()
+    @IsString()
     readonly title: string;
+
+    @IsOptional()
+    @IsString()
     readonly description: string;
+
+    @IsOptional()
+    @IsString()
     readonly author: string;
+
+    @IsOptional()
+    @IsNumber()
     readonly price: number;
+
+    @IsOptional()
+    @IsEnum(Category,{message:'please enter the correct enum'})
     readonly category: Category
 
 }
