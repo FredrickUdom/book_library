557482cd968a163b4d18727e7f8a7fc99e9de598
“fredrickUdom” <fredrickudom@gmail.com>
2023-05-22 12:05:35 +0100

    adding password strong

--- src/auth/dto/login.Dto.ts
+++ src/auth/dto/login.Dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString, Matches, MinLength } from "class-validator";
 
 export class loginDto{
     @IsEmail({},{message: 'please enter a valid email address'})
@@ -7,5 +7,6 @@ export class loginDto{
     @IsNotEmpty()
     @IsString()
     @MinLength(6)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {message: 'password must contain atleast one upperCase, one number and special character'})
     readonly password: string;
 }
--- src/auth/dto/signupDto.ts
+++ src/auth/dto/signupDto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString, Matches, MinLength } from "class-validator";
 
 export class signUpDto{
     @IsNotEmpty()
@@ -12,5 +12,6 @@ export class signUpDto{
     @IsNotEmpty()
     @IsString()
     @MinLength(6)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {message: 'password must contain atleast one upperCase, one number and special character'})
     readonly password: string;
 }
